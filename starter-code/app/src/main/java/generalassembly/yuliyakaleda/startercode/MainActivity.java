bc8c20ca0c074a20fe803f009fa07c65fe6f3d55
roberrera <roberterrera@gmail.com>
2016-02-21 18:07:06 -0500

    Enable long-click to delete

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
   Button mButton;
   ArrayList<String> mWishList;
   ArrayAdapter<String> mAdapter;
+    Animation mAnimation;
 
 
   @Override
@@ -30,46 +32,55 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     mEditText = (EditText) findViewById(R.id.edit_text);
-
     mTextView = (TextView) findViewById(R.id.textview);
     mListView = (ListView) findViewById(R.id.listview_wishes);
     mButton = (Button) findViewById(R.id.button_add);
+
     mWishList = new ArrayList<>();
-    mAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mWishList);
+    mAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, mWishList);
     mListView.setAdapter(mAdapter);
 
+      mAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
+
+      // Give the listView an animated transition.
+      LayoutTransition layout = new LayoutTransition();
+      layout.enableTransitionType(LayoutTransition.CHANGING);
+      mListView.setLayoutTransition(layout);
+
     mButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        //       1. get the text from the input field
-          String userInput = mEditText.getText().toString();
+        @Override
+        public void onClick(View v) {
+            //       1. get the text from the input field
+            //       2. animate it in the center of the screen
+            //       3. add it to the wish list
+            //       4. clear the input field
 
-        //       2. animate it in the center of the screen
+            String userInput = mEditText.getText().toString();
+            mTextView.setText(userInput);
 
-          mTextView.setText(userInput);
-          LayoutTransition layout = new LayoutTransition();
-          layout.enableTransitionType(LayoutTransition.CHANGING);
-          mListView.setLayoutTransition(layout);
+            mTextView.startAnimation(mAnimation);
 
-          Animation textAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
-          mTextView.startAnimation(textAnimation);
+            mWishList.add(0, userInput);
+            mAdapter.notifyDataSetChanged();
 
-        //       3. add it to the wish list
-          mWishList.add(0, userInput);
-          mAdapter.notifyDataSetChanged();
-
-        //       4. clear the input field
-          mEditText.setText("");
-      }
+            mEditText.setText(null);
+//          mTextView.setText(null);
+        }
     });
 
+      mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+          @Override
+          public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+              if (!mWishList.isEmpty()) {
+                  mWishList.remove(position);
+                  mAdapter.notifyDataSetChanged();
+                  return true;
+              } else {
+                  return false;
+              }
+          }
+      });
+
   }
-
-    public void add(int index, String wish){
-        String userInput = mEditText.getText().toString();
-
-
-    }
-
 
 }
